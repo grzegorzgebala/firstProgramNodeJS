c9f134a783926078534a96ce219c5e5794fc4a96
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-10-03 13:36:20 +0200

    Modification getOSinfo

--- program.js
+++ program.js
@@ -1,21 +1,43 @@
-process.stdin.setEncoding('utf-8');
+var os = require('os');
 
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
-    // tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
-   	var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
                 process.exit();
-            break;
-            case 'version':
-                process.stdout.write(process.version + '\n');
-            break;
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            case '/getOSinfo':
+                getOSinfo();
+                break;
             default:
-                process.stderr.write('Wrong instruction!' + '\n');
-
-        }
+                process.stderr.write('Wrong instruction!\n');
+        };
     }
 });
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
