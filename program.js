c16f0506cc21f6a79f60f8f90d737b94a036a1f2
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-10-03 07:43:49 +0200

    First task finish

--- program.js
+++ program.js
@@ -5,11 +5,17 @@ process.stdin.on('readable', function() {
    	var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!');
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+            break;
+            case 'version':
+                process.stdout.write(process.version + '\n');
+            break;
+            default:
+                process.stderr.write('Wrong instruction!' + '\n');
+
         }
     }
 });
