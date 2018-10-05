1b2a373e2df722ae057a202ece5ac100ecef051f
Michal Gacek <gacek.michael@outlook.com>
2018-10-06 00:11:49 +0200

    Fixes

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -13,7 +13,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    OStime.print();
+    OStime.print(os.uptime());
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
